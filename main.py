7951dc9c313719d01ab7b16f1787c533f95cd221
Nikolay Chechelev <nikolaychechelev@gmail.com>
2020-09-28 21:58:19 +0400

    pygame installation fix

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 if __name__ == "__main__":
     import os
     import time
+    try:
+        import pygame
+    except:
+        os.system('pip3 install pygame')
     time.sleep(1)
     cmd = os.system('git -C "/home/pi/MCN-12" pull origin master')
     time.sleep(5)
