c54d2025190867687c162af22ef46a2fa29bb3e6
Nikolay Chechelev <nikolaychechelev@gmail.com>
2020-09-28 23:23:13 +0400

    pygame installation fix

--- libs/xx74xx165_driver.py
+++ libs/xx74xx165_driver.py
@@ -1,5 +1,18 @@
-import smbus
+import spidev
 
 class xx74xx165_driver:
-    def __init__(self):
+    def __init__(self, bus=0, device=1):
+        self.bus = bus
+        self.device = device
+        self.spi = spidev.SpiDev()
+        self.spi.open(self.bus, self.device)
+        self.spi.max_speed_hz = 1000000
+        self.spi.mode = 0
         pass
+
+    def read_chip(self, pin):
+        pass
+
+    def reset_chip(self):
+        pass
+
--- libs/xx74xx595_driver.py
+++ libs/xx74xx595_driver.py
@@ -1,5 +1,20 @@
-import smbus
+import spidev
 
 class xx74xx595_driver:
-    def __init__(self):
+    def __init__(self, bus=0, device=1):
+        self.bus = bus
+        self.device = device
+        self.spi = spidev.SpiDev()
+        self.spi.open(self.bus, self.device)
+        self.spi.max_speed_hz = 1000000
+        self.spi.mode = 0
+        pass
+
+    def set_pin(self, pin):
+        pass
+
+    def reset_pin(self, pin):
+        pass
+
+    def reset_chip(self):
         pass
