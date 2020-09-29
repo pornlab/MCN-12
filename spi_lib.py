3f0999a4c71d47209f18e016c89ce7ab47b94ae1
Nikolay Chechelev <nikolaychechelev@gmail.com>
2020-09-29 16:25:49 +0400

    pygame installation fix

--- spi_lib.py
+++ spi_lib.py
@@ -1,7 +1,7 @@
 import spidev
 
 class SPI:
-    def __init__(self, bus=0, device=1):
+    def __init__(self):
         self.config = eval(' '.join(open('config.txt', 'r').read().split('\n')))
         self.bus = self.config['spi']['bus']
         self.device = self.config['spi']['device']
@@ -9,6 +9,10 @@ class SPI:
         self.spi.open(self.bus, self.device)
         self.spi.max_speed_hz = self.config['spi']['max_speed_hz']
         self.spi.mode = self.config['spi']['spi_mode']
+        self.modules = self.config['modules']
+
+        self.read_cmd = [0] * self.modules
+        self.set_cmd = [0] * self.modules
         pass
 
 
