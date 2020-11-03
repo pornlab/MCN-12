36bd9dde73d2219a8fda277d60cbf5e5df2ccf2a
Nikolay Chechelev <nikolaychechelev@gmail.com>
2020-11-03 20:20:26 +0400

    qwer

--- spi_lib.py
+++ spi_lib.py
@@ -33,17 +33,17 @@ class SPI:
         sum = 0
         for i in range(1, len(self.data)):
             if self.data[i] > 15:
-                sum += (math.log2(self.data[i]) - 3) + i * 4
+                sum += (math.log2(self.data[i]) - 3) + i * 4 - 5
                 self.wall_data[i] = self.data[i]
-        return sum
+        return int(sum)
 
     def floor_num(self):
         sum = 0
         for i in range(1, len(self.data)):
             if 0 < self.data[i] < 16:
-                sum += (math.log2(self.data[i])) + i * 4
+                sum += (math.log2(self.data[i])) + i * 4 - 4
                 self.floor_data[i] = self.data[i]
-        return sum
+        return int(sum)
 
     def get_timeout(self):
         return self.config['timeout'] * 25
