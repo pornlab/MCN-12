2e7a23eca4bfce39263924e5e4363393b17ac872
Nikolay Chechelev <nikolaychechelev@gmail.com>
2020-11-03 22:48:51 +0400

    K

--- spi_lib.py
+++ spi_lib.py
@@ -101,7 +101,7 @@ class SPI:
             self.room = 0
             self.out = [0] * self.modules
 
-        self.image_path = os.path.join('images', 'room\ {}'.format(self.room), 'wall\ {}'.format(self.wall),
+        self.image_path = os.path.join('images', 'room {}'.format(self.room), 'wall {}'.format(self.wall),
                                        '{}.png'.format(self.floor))
         print(self.image_path)
         return self.image_path
