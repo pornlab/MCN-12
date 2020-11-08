b811817ed118149160e1e4c3a2c552ae1150313c
Nikolay Chechelev <nikolaychechelev@gmail.com>
2020-11-09 00:05:11 +0400

    K

--- graphics.py
+++ graphics.py
@@ -1,6 +1,7 @@
 import pygame
 import os
-from PIL import Image
+from moviepy.editor import VideoFileClip as VC
+import ctypes
 
 
 class Graphics:
@@ -13,26 +14,36 @@ class Graphics:
         self.screen = self.display.set_mode(size=[self.scr_w, self.scr_h])#, flags=pygame.FULLSCREEN, display=0)
         self.screen.fill([0, 0, 0])
         print(pygame.image.get_extended())
+        self.path = "videos/1080_60fps.mp4"
+
 
     def load_image(self, image_path=os.path.join('images', 'room_0', 'wall_0', '0.png')):
-        # try:
-        #     img = pygame.image.load(image_path)
-        # except:
-        #     f = Image.open(image_path)
-        #     f = f.save(os.path.join('images', '{}.bmp'.format(image_num)))
-        #     img = pygame.image.load(os.path.join('images', '{}.bmp'.format(image_num)))
-
+        clip = VC(self.path)
         try:
             img = pygame.image.load(image_path)
             self.screen.blit(img, [0, 0])
+            if '0.png' in image_path:
+                clip.preview()
+            else:
+                clip.close()
+            pygame.display.flip()
         except:
-            print(image_path)
+            pass
+
+
 
         self.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                clip.close()
                 pygame.quit()
                 break
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                clip.close()
                 pygame.quit()
                 break
+
+
+g = Graphics()
+while 1:
+    g.load_image()
--- main.py
+++ main.py
@@ -3,16 +3,28 @@ if __name__ == "__main__":
     import time
 
     try:
-        #os.system('pip3 install -U pygame==2.0.0.dev12')
         import pygame
     except:
-
+        os.system('pip3 install -U pygame==2.0.0.dev12')
         import pygame
+
     try:
         import RPi.GPIO as IO
     except:
         os.system('pip3 install RPi.GPIO')
         import RPi.GPIO as IO
+
+    try:
+        import moviepy
+    except:
+        os.system('pip3 install moviepy')
+        import pyglet
+
+    try:
+        import ctypes
+    except:
+        os.system('pip3 install ctypes')
+        import ctypes
 
     time.sleep(1)
     cmd = os.system('git -C "/home/pi/MCN-12" pull origin master')
@@ -20,14 +32,3 @@ if __name__ == "__main__":
 
     from routine import Routine
     r = Routine()
-
-# import os
-# from PIL import Image
-# for room in range(6):
-#     for wall in range(13):
-#         for floor in range(13):
-#             image_path = os.path.join('images', 'room\ {}'.format(room), 'wall\ {}'.format(wall), '{}'.format(floor))
-#             print(image_path)
-#             os.system('rm ' + image_path+'.bmp')
-#             # f = Image.open(image_path+'.png')
-#             # f = f.save(image_path+'.bmp')
