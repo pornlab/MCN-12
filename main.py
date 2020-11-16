112a0c15bf35f25937a71bb447b2f5781edb4b09
Nikolay Chechelev <nikolaychechelev@gmail.com>
2020-11-17 00:33:14 +0400

    K

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ if __name__ == "__main__":
     try:
         import pygame
     except:
-        os.system('pip3 install -U pygame==1.9.6')
+        os.system('sudo apt-get install python-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev '
+                  'libsdl1.2-dev libsmpeg-dev python-numpy subversion libportmidi-dev ffmpeg libswscale-dev '
+                  'libavformat-dev libavcodec-dev libfreetype6-dev')
+        os.system('pip3 install -U pygame==1.9.2')
         import pygame
 
     try:
