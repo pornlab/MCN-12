3e2f65197d6efc9c7792effae81e742a9b8b1dad
Nikolay Chechelev <nikolaychechelev@gmail.com>
2020-12-13 16:53:03 +0400

    New video for conference

--- spi_lib.py
+++ spi_lib.py
@@ -96,7 +96,7 @@ class SPI:
             self.wall = 0
             self.floor = 0
             self.room = 0
-            self.out = [0] * self.modules
+            #self.out = [0] * self.modules
             if not self.video_conf:
                 self.out = [0] * self.modules
             else:
